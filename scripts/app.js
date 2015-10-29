884600b42578890db58907fbf194d61e231d985a
David Beeler <david.beeler@gmail.com>
2015-10-29 16:23:01 -0500

    added some more js ability

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,14 @@
 window.onload = function() {
   //alert('hey hey hey');
   statusReport("hey hey hey");
+
+  var username = prompt("What is your name?");
+  var mission = prompt("What is your mission?");
+
+  var contentContainer = document.getElementById("content");
+
+  contentContainer.innerHTML = username + " is on a mission to " + mission;
+
 }
 
 // Send up messages
