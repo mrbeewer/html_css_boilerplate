7cc6fbf41ec55d6c3963184e84571e4c524cc9d7
David Beeler <david.beeler@gmail.com>
2015-10-29 16:07:09 -0500

    initial app.js info added

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+// Wait until the window has loaded
+window.onload = function() {
+  //alert('hey hey hey');
+  statusReport("hey hey hey");
+}
+
+// Send up messages
+function statusReport(message) {
+  console.log(message);
+  return message;
+}
